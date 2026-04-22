e6bf387b6158ff2922ce50287c89997325bd3890
Matteo Alderighi <alderighi@het.it>
2026-04-22 12:03:45 +0200

    upgrade to angular 20

--- src/lib/my-prime-table.module.ts
+++ src/lib/my-prime-table.module.ts
@@ -5,7 +5,7 @@ import { CommonModule } from '@angular/common';
 
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
-import { InputSwitchModule } from 'primeng/inputswitch';
+import { ToggleSwitchModule } from 'primeng/toggleswitch';
 import { InputTextModule } from 'primeng/inputtext';
 import { PaginatorModule } from 'primeng/paginator';
 import { PanelModule } from 'primeng/panel';
@@ -17,25 +17,25 @@ import { TableModule } from 'primeng/table';
   declarations: [
     MyPrimeTableComponent
   ],
-  imports: [
-    TableModule,
-    CommonModule,
-    ButtonModule,
-    PaginatorModule,
-    InputTextModule,
-    PanelModule,
-    InputSwitchModule,
-    FormsModule,
-    //BrowserAnimationsModule,
-  ],
-  exports: [
-    MyPrimeTableComponent,
-    TableModule,
-    ButtonModule,
-    PaginatorModule,
-    InputTextModule,
-    PanelModule,
-    InputSwitchModule,
-  ],
+   imports: [
+     TableModule,
+     CommonModule,
+     ButtonModule,
+     PaginatorModule,
+     InputTextModule,
+     PanelModule,
+     ToggleSwitchModule,
+     FormsModule,
+     //BrowserAnimationsModule,
+   ],
+   exports: [
+     MyPrimeTableComponent,
+     TableModule,
+     ButtonModule,
+     PaginatorModule,
+     InputTextModule,
+     PanelModule,
+     ToggleSwitchModule,
+   ],
 })
 export class MyPrimeTableModule { }
